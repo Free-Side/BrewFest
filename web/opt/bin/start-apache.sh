2d6ff246a6c011f0fcd0cfed819729241f184502
Paul Wheeler <paul@invioinc.com>
2018-08-13 08:48:36 -1000

    Added php5-curl package. use mapped volumes for msmtp config and external php pages. upgrade to latest alph release of BCOE.

--- web/opt/bin/start-apache.sh
+++ web/opt/bin/start-apache.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+# msmtp doesn't like it's config to be accessible to non-owners
+chmod go-rwx /etc/msmtp.conf/msmtprc
+chown www-data /etc/msmtp.conf/msmtprc
+
+touch /var/log/msmtp.log
+chgrp www-data /var/log/msmtp.log
+chmod g+w /var/log/msmtp.log
+
 rm -f "$APACHE_PID_FILE"
 /usr/sbin/apache2 &
 tail -f $APACHE_LOG_DIR/*
